172b181561b536fac133dff531a1baeb2ee3e86d
haodongxi <haodongsheng@haodongshengdeMacBook-Air.local>
2020-12-03 23:28:03 +0800

    16最近三数之和未完成

--- 16.py
+++ 16.py
@@ -1,5 +1,40 @@
 from typing import List
 def threeSumClosest( nums: List[int], target: int) -> int: 
     nums.sort()
+    i = 0
+    lastSum = None
+    while i < len(nums)-2:
+        l = i+1
+        r = len(nums)-1
+        subLastSum = None
+        lastUp = None
+        ch = None
+        while l < r:
+            nowSum = nums[i]+nums[l]+nums[r]
+            nowCh = target - nowSum
+            nowUp = None
+            if  nowSum == target:
+                return target
+            elif (subLastSum == None and nowCh>0) or  (ch!=None and nowCh>ch):
+                r = r-1
+                nowUp = False
+            else:
+                l = l+1
+                nowUp = True
+            if lastUp==None or lastUp == nowUp:
+                lastUp = nowUp
+                subLastSum = nowSum
+                ch = nowCh
+            else:
+                if nowSum>subLastSum:
+                    break
+                else :
+                    subLastSum = nowSum
+                    break
+        if lastSum==None or (((target - subLastSum)< (target - lastSum)) if ((target - subLastSum)*(target - lastSum))<0 else (abs(target - subLastSum)< abs(target - lastSum))):
+            lastSum = subLastSum
+        i = i+1
+    return lastSum
+
 if __name__ == "__main__":
-    print(threeSumClosest([-1,2,1,-4],2))
+    print(threeSumClosest([1,1,1,0],-100))
--- test.py
+++ test.py
@@ -1,7 +1,12 @@
-nums = [2,0,0]
-d = {}
-r = 1
-print ( True if len(nums)-1==r else (True if nums[r]!=nums[r+1] else False))
-print(str("1"))
-a = "1,2,3"
-print(a.split(","))
+# nums = [2,0,0]
+# d = {}
+# r = 1
+# print ( True if len(nums)-1==r else (True if nums[r]!=nums[r+1] else False))
+# print(str("1"))
+# a = "1,2,3"
+# print(a.split(","))
+# 1
+# [-4,-1,1,2]
+a = None
+a = 1
+print(a)
