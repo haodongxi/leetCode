5e2dbbad59ecf53b42e0159fde1d143f8d6b2c5d
haodongxi <haodongsheng@haodongshengdeMacBook-Air.local>
2020-12-07 23:03:57 +0800

    17 电话号码的字母组合

--- 17.py
+++ 17.py
@@ -1,4 +1,15 @@
+from typing import List
 def letterCombinations(digits: str) -> List[str]:
-    pass
+    mDict = {'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
+    if len(str) == 0:
+        return []
+    if len(digits) == 1:
+        return  [a for a in mDict[digits]]
+    result  = []
+    for a in mDict[digits[0]]:
+        for x in letterCombinations(digits[1:]):
+             result.append(a+x)
+    return result
+
 if __name__ == "__main__":
-    pass
+    print(letterCombinations('1'))
