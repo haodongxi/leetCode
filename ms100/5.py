5a95b8e79db015b2e5d2e58f65ef88c8d23e75d9
haodongsheng <haodongsheng@live.cn>
2025-06-17 00:48:08 +0800

    数组4，5

---
+++ ms100/5.py
@@ -0,0 +1,38 @@
+# 给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+
+# 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+
+ 
+
+# 示例 1：
+
+# 输入：nums = [3,2,3]
+# 输出：3
+# 示例 2：
+
+# 输入：nums = [2,2,1,1,1,2,2]
+# 输出：2
+ 
+
+# 提示：
+# n == nums.length
+# 1 <= n <= 5 * 104
+# -109 <= nums[i] <= 109
+
+from typing import List
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        count_map = {}
+        for i in range(n):
+            value = nums[i]
+            count_map[value] = (count_map[value] if value in count_map.keys() else 0 )+1
+            if count_map[value] > n//2:
+                return value
+        return None
+    
+    def majorityElement2(self, nums: List[int]) -> int:
+        nums.sort()
+        nums[len(nums)//2]
